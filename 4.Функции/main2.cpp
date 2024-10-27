da2efdab1d881e5e819eb67ea7c314bfeca171c4
separeit894 <rinatramazanov894@gmail.com>
2024-10-28 00:30:35 +0300

    full_quare_ task

--- 4.Функции/main2.cpp
+++ 4.Функции/main2.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cmath>
 
-using namespace std;
+using std::cin, std::endl, std::cout;
 
-int main() {
+float calculatey(float a) 
+{
+    if (a > 360) 
+    {
+        cout << "Угол больше 360" << endl;
+        return 1;
+    } else if (a < -360) 
+    {
+        cout << "Угол меньше чем -360" << endl;
+        return 1;
+    }
+    // Перевод угла в радианы
+    float a_radians = a * M_PI / 180.0;
+    float Y = tan(2.0 * a_radians) * cos(2.0 * a_radians); 
     
+    return Y;
+}
+
+int main() 
+{
+    float g = calculatey(70);
+    cout << g << endl;
 }
